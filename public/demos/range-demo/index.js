d24f47c5784bf485ac7adfb5bb2b066fbc58be23
dustinpfister <dustin.pfister@gmail.com>
2018-01-23 11:35:55 -0500

    range method and demo

---
+++ public/demos/range-demo/index.js
@@ -0,0 +1,50 @@
+forFrame({
+
+    name: 'basic-box-demo',
+
+    maxFrame: 100,
+
+    // the init method is called once to set things up
+    init: function () {
+
+        this.addDisp({
+            id: 'box',
+            type: 'graphics',
+            forFrame: function (ff) {
+                var gfx = this.disp;
+                gfx.clear();
+                gfx.beginFill(0x0000ff);
+                gfx.drawRect(0, 0, 32, 32);
+                gfx.endFill();
+            }
+        });
+
+    },
+
+    forFrame: function () {
+
+        game = this.game;
+
+        var bx = this.get('box').disp;
+
+        // this is what will happen by default
+        bx.x = 0;
+        bx.y = 100 * this.per;
+
+        // this is what will happen for 25% to 75% of the frames
+        this.range({
+
+            start: .25,
+            end: .75,
+
+            forFrame: function () {
+
+                bx.x = 100-100 * this.seg.bias;
+
+            }
+
+        });
+
+    }
+
+});
--- public/forframe-client.js
+++ public/forframe-client.js
@@ -69,129 +69,160 @@ var forFrame = (function () {
                     }
                 }));
 
-        }
+        },
 
-    },
+        range: function (argv) {
 
-    // play / pause
-    play = function (ff) {
+            argv = argv || {};
+            argv.start = argv.start || 0;
+            argv.end = argv.end || 1;
+            argv.forFrame = argv.forFrame || function () {};
 
-        ff.play = !ff.play;
+            // set segment values
+            let maxFrame = this.maxFrame * (argv.end - argv.start),
+            frame = this.frame % maxFrame,
+            per = frame / maxFrame;
 
-    },
+            if (this.per >= argv.start && this.per <= argv.end) {
 
-    // tick the animation
-    tick = function (ff) {
+                argv.forFrame.call(_.merge({}, this, {
 
-        // step fram index
-        ff.frame += 1;
-        if (ff.frame >= ff.maxFrame) {
+                        seg: {
 
-            ff.frame = 0;
+                            frame: Math.floor(frame),
+                            maxFrame: Math.floor(maxFrame),
+                            per: per,
+                            bias: 1 - Math.abs(.5 - per) / .5
 
-        }
+                        }
 
-    };
-
-    // return the method that will be called when making the animation
-    return function (ff) {
-
-        // set starting values
-        ff.frame = 0;
-        ff.maxFrame = ff.maxFrame || 50;
-        ff.per = 0;
-        ff.bias = 0;
-
-        ff.obj = [];
-        ff.play = true;
-        ff.width = ff.width || 320;
-        ff.height = ff.height || 240;
-        ff.name = ff.name || 'untitled';
-
-        // these should be given, but default to noop anyway
-        ff.init = ff.init || function () {};
-        ff.forFrame = ff.forFrame || function () {};
-
-        // using phaser
-        ff.game = new Phaser.Game(ff.width, ff.height + 50, Phaser.AUTO, container, {
-
-                // phaser create state method
-                create: function () {
-
-                    // call the animations ini method here
-                    ff.init.call(_.merge(ff, initAPI));
-
-                    // be sure to do this so phaser does not
-                    // scroll to the top on mobile
-                    ff.game.scale.compatibility.scrollTo = false;
-
-                    // play button
-                    var button_play = this.game.add.graphics(0, ff.height);
-                    button_play.beginFill(0x00ff00);
-                    button_play.drawRect(0, 0, 50, 50);
-                    button_play.endFill();
-                    button_play.inputEnabled = true;
-                    button_play.events.onInputDown.add(function () {
-
-                        play(ff);
-
-                    });
-
-                    // tick button
-                    var button_tick = this.game.add.graphics(0, ff.height);
-                    button_tick.beginFill(0x0000ff);
-                    button_tick.drawRect(50, 0, 50, 50);
-                    button_tick.endFill();
-                    button_tick.inputEnabled = true;
-                    button_tick.events.onInputDown.add(function () {
-
-                        tick(ff);
-
-                    });
-
-                },
-
-                // phaser update method
-                update: function () {
-
-                    // set per, and bias values for the current frame
-                    ff.per = ff.frame / ff.maxFrame;
-                    ff.bias = 1 - Math.abs(.5 - ff.per) / .5;
-
-                    // call main for frame method
-                    ff.forFrame.call(_.merge({}, ff, ffAPI));
-
-                    // call for frame methods for all display objects
-                    var i = 0,
-                    len = ff.obj.length;
-                    while (i < len) {
-
-                        //ff.obj[i].forFrame.call(ff.obj[i].disp, ff);
-
-                        ff.obj[i].forFrame.call(
-
-                            _.merge({}, ff, ffAPI, {
-
-                                obj: ff.obj[i],
-                                disp: ff.obj[i].disp
-
-                            }));
-
-                        i += 1;
-
-                    }
-
-                    if (ff.play) {
-
-                        tick(ff);
-
-                    }
+                    }));
 
                 }
 
-            }, true);
+            }
 
-    };
+        },
 
-}
-    ());
+        // play / pause
+        play = function (ff) {
+
+            ff.play = !ff.play;
+
+        },
+
+        // tick the animation
+        tick = function (ff) {
+
+            // step fram index
+            ff.frame += 1;
+            if (ff.frame >= ff.maxFrame) {
+
+                ff.frame = 0;
+
+            }
+
+        };
+
+        // return the method that will be called when making the animation
+        return function (ff) {
+
+            // set starting values
+            ff.frame = 0;
+            ff.maxFrame = ff.maxFrame || 50;
+            ff.per = 0;
+            ff.bias = 0;
+
+            ff.obj = [];
+            ff.play = true;
+            ff.width = ff.width || 320;
+            ff.height = ff.height || 240;
+            ff.name = ff.name || 'untitled';
+
+            // these should be given, but default to noop anyway
+            ff.init = ff.init || function () {};
+            ff.forFrame = ff.forFrame || function () {};
+
+            // using phaser
+            ff.game = new Phaser.Game(ff.width, ff.height + 50, Phaser.AUTO, container, {
+
+                    // phaser create state method
+                    create: function () {
+
+                        // call the animations ini method here
+                        ff.init.call(_.merge(ff, initAPI));
+
+                        // be sure to do this so phaser does not
+                        // scroll to the top on mobile
+                        ff.game.scale.compatibility.scrollTo = false;
+
+                        // play button
+                        var button_play = this.game.add.graphics(0, ff.height);
+                        button_play.beginFill(0x00ff00);
+                        button_play.drawRect(0, 0, 50, 50);
+                        button_play.endFill();
+                        button_play.inputEnabled = true;
+                        button_play.events.onInputDown.add(function () {
+
+                            play(ff);
+
+                        });
+
+                        // tick button
+                        var button_tick = this.game.add.graphics(0, ff.height);
+                        button_tick.beginFill(0x0000ff);
+                        button_tick.drawRect(50, 0, 50, 50);
+                        button_tick.endFill();
+                        button_tick.inputEnabled = true;
+                        button_tick.events.onInputDown.add(function () {
+
+                            tick(ff);
+
+                        });
+
+                    },
+
+                    // phaser update method
+                    update: function () {
+
+                        // set per, and bias values for the current frame
+                        ff.per = ff.frame / ff.maxFrame;
+                        ff.bias = 1 - Math.abs(.5 - ff.per) / .5;
+
+                        // call main for frame method
+                        ff.forFrame.call(_.merge({}, ff, ffAPI));
+
+                        // call for frame methods for all display objects
+                        var i = 0,
+                        len = ff.obj.length;
+                        while (i < len) {
+
+                            //ff.obj[i].forFrame.call(ff.obj[i].disp, ff);
+
+                            ff.obj[i].forFrame.call(
+
+                                _.merge({}, ff, ffAPI, {
+
+                                    obj: ff.obj[i],
+                                    disp: ff.obj[i].disp
+
+                                }));
+
+                            i += 1;
+
+                        }
+
+                        if (ff.play) {
+
+                            tick(ff);
+
+                        }
+
+                    }
+
+                }, true);
+
+        };
+
+    }
+        ());
