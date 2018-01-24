abb12f941a5d3b646d63805dd323497fc7bfe594
dustinpfister <dustin.pfister@gmail.com>
2018-01-24 10:20:03 -0500

    new build script

---
+++ scripts/build.js
@@ -0,0 +1,36 @@
+
+let fs = require('fs-extra'),
+klaw = require('klaw'),
+path = require('path'),
+min = require('uglify-js');
+
+klaw('./src').on('data', function (item) {
+
+    if (item.stats.isFile()) {
+
+        fs.ensureDir('./dist').then(function () {
+
+            return fs.readFile(item.path, 'utf-8')
+
+        }).then(function (data) {
+
+            let ext = path.extname(item.path),
+            fn = path.basename(item.path, ext),
+            fn_target = fn + '.min' + ext,
+            dir = path.join('./dist', fn_target);
+
+            return fs.writeFile(dir, 'utf-8');
+
+        }).then(function () {
+
+            console.log('built: ' + item.path);
+
+        }).catch (function (e) {
+
+            console.log(e);
+
+        });
+
+    }
+
+});
